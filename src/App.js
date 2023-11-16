19207e3944b36f81076f0da475e7881a8202a29e
sjvolesky <131320763+sjvolesky@users.noreply.github.com>
2023-11-16 12:22:47 -0700

    Created a search bar, added css, added footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Samantha Muckridge open-sourced on GitHub</small>
+        </footer>
+      </div>
     </div>
   );
 }
