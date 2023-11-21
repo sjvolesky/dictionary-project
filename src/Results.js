5ddd6e73e80590c1f8490a8a06351ce5e3ac7193
sjvolesky <131320763+sjvolesky@users.noreply.github.com>
2023-11-21 14:00:21 -0700

    organized the results.js

--- src/Results.js
+++ src/Results.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Meaning from "./Meaning";
 import Phonetic from "./Phonetic";
+import Meaning from "./Meaning";
 
 export default function Results(props) {
   if (props.results) {
