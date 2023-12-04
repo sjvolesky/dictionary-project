11167ceb7ddec736bbc86001b067f785d1476645
sjvolesky <131320763+sjvolesky@users.noreply.github.com>
2023-12-04 11:11:36 -0700

    added styling

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
       <div className="container">
         <header className="App-header"></header>
         <main>
-          <Dictionary />
+          <Dictionary defaultKeyword="ocean" />
         </main>
         <footer className="App-footer">
           <small>Coded by Samantha Muckridge open-sourced on GitHub</small>
