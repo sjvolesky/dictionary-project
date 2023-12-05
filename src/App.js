9da55cc0d3fcfa8592c7934f5b8e72b727870b44
sjvolesky <131320763+sjvolesky@users.noreply.github.com>
2023-12-04 20:26:43 -0700

    added photos api

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ export default function App() {
       <div className="container">
         <header className="App-header"></header>
         <main>
-          <Dictionary defaultKeyword="ocean" />
+          <Dictionary defaultKeyword="sunset" />
         </main>
         <footer className="App-footer">
-          <small>Coded by Samantha Muckridge open-sourced on GitHub</small>
+          <small>Samantha Muckridge open-sourced on GitHub</small>
         </footer>
       </div>
     </div>
